6e66a608855636fec342f5cd81385df2ec6b1700
eggtgg <mtri.egg@gmail.com>
2021-01-11 20:21:26 +0700

    Merge remote-tracking branch 'origin/master'

--- scrapy.py
+++ scrapy.py
@@ -1,15 +1,44 @@
-'''import re
-a='hello minh tri hồ óc chó vcl'
-k=re.findall('(?:hello{6})*',a)
-print(k)
-#how are you today?
-#hello ae'''
 import requests
 from bs4 import BeautifulSoup
-import csv
-import json
-r = requests.get('https://authoraditiagarwal.com/')
-soup = BeautifulSoup(r.text, 'lxml')
-y = json.dumps(soup.title.text)
-with open('JSONFile.txt', 'wt') as outfile:
-   json.dump(y, outfile)
+
+
+#1 HÀM REQUESTS LINK VÀ TÌM LINK MỚI ADD VÀO
+def request_url(url_text):
+    l = requests.get(url_text)
+    return l
+
+
+#2 HÀM PHÂN TÍCH DƯỚI DẠNG BEATIFULSUOP VÀ TRẢ VỀ CÁC URL LIÊN QUAN
+def phan_tich(request_result):
+    link_html = BeautifulSoup(request_result.text, 'html.parser')
+    list_url_lien_quan = link_html('div', class_="clearfix item")
+    return list_url_lien_quan
+
+
+#3 HÀM DOWNLOAD LINK
+def download_link(html_text):
+    file = open('file' + str() + '.html', 'w')
+    file.write(html_text)
+    file.close()
+    pass
+
+def ham4(list_result_phan_tich):
+    for a_i in list_result_phan_tich:
+        linkrq ='https://vietnamnet.vn'+ a_i.find('a')['href']
+        if linkrq in s:
+            continue
+        else:
+            s.append(linkrq)
+            html = request_url(linkrq)
+            download_link(html.text)
+
+def ham5(url):
+    k = request_url(url)
+    h = phan_tich(k)
+    ham4(h)
+
+
+s = [
+    'https://vietnamnet.vn/vn/thoi-su/an-toan-giao-thong/xe-limousine-gay-tai-nan-tham-khoc-do-phong-nhanh-vuot-au-699177.html']
+for s_i in s:
+    ham5('https://vietnamnet.vn/vn/thoi-su/an-toan-giao-thong/xe-limousine-gay-tai-nan-tham-khoc-do-phong-nhanh-vuot-au-699177.html')
